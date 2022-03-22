ae41c97c9bafa480646f888810185c8c5cff259b
kevin <kevin345618@outlook.com>
2022-03-22 21:53:25 +0800

    prod setting

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
-const fs = require("fs");
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    https: {
-      key: fs.readFileSync(`${__dirname}/localhost-key.pem`),
-      cert: fs.readFileSync(`${__dirname}/localhost.pem`),
-    },
-    port: "8081",
-  },
 });
