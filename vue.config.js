5c3e6cc7d87e8c863a7b62cb0d0f2fdfa35cfc18
kevin <kevin345618@outlook.com>
2022-03-27 00:02:50 +0800

    fix build issue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
+const fs = require("fs");
 const { defineConfig } = require("@vue/cli-service");
+const { env } = require("process");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    https:
+      env.NODE_ENV === "production"
+        ? {}
+        : {
+            key: fs.readFileSync(`${__dirname}/localhost-key.pem`),
+            cert: fs.readFileSync(`${__dirname}/localhost.pem`),
+          },
+    port: "8081",
+  },
 });
